b1cdd57da34492ca681213e18e3857c75648ce2e
Jan Valušek <janvalusek123@gmail.com>
2025-02-02 14:05:56 +0100

    Improve streaming script

--- rpi/stream.sh
+++ rpi/stream.sh
@@ -3,5 +3,5 @@
 URL="rtmp://134.122.67.47/live/stream"
 
 rpicam-vid -t 0 --inline --framerate 30 --width 1280 --height 720 --codec h264 \
---profile high --level 4.2 --bitrate 1500000 -o - | \
+--profile high --level 4.2 --bitrate 3500000 -o - | \
 ffmpeg -re -f h264 -i - -c:v copy -f flv $URL
